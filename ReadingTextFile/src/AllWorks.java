230760b4bf3c463861064bb072d1a4b41fbeeff8
fabian <fw031@hdm-stuttgart.de>
2016-04-27 10:11:34 +0200

    added getAllSpeakers

---
+++ ReadingTextFile/src/AllWorks.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author kai
+ */
+public class AllWorks {
+    private List<Work> works = new ArrayList<>();
+
+    public void add(Work work) {
+        works.add(work);
+    }
+
+    public void addAll(AllWorks aw) {
+        works.addAll(aw.getAll());
+    }
+
+    public List<Work> getAll() {
+        return works;
+    }
+
+    public List<Speaker> getAllSpeakers() {
+        List<Speaker> res = new ArrayList<>();
+        for (Work w: works) {
+            res.addAll(w.getSpeakers());
+        }
+        Collections.sort(res, (speaker1, speaker2) -> speaker2.getNumberOfMonologues()-speaker1.getNumberOfMonologues());
+        return res;
+    }
+
+
+}
